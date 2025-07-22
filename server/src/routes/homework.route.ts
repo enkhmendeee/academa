c7f8d006160a2b69c8251fce196a9c03bfbcd269
enkhmendeee <nergui.eegii04@gmail.com>
2025-07-22 18:15:17 +0800

    Added homework.route

---
+++ server/src/routes/homework.route.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  getHomeworks,
+  createHomework,
+  updateHomework,
+  deleteHomework,
+} from "../controllers/homework.controller";
+import { authenticateToken } from "../middleware/authMiddleware";
+
+const router = express.Router();
+
+router.use(authenticateToken);
+
+router.get("/", getHomeworks);
+router.post("/", createHomework);
+router.patch("/:id", updateHomework);
+router.delete("/:id", deleteHomework);
+
+export default router;
