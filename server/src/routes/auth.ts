af1d868f1af4bb79bb5040b0b467e565d88a6c1e
enkhmendeee <nergui.eegii04@gmail.com>
2025-07-22 11:20:55 +0800

    added login route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 import { PrismaClient } from '../generated/prisma';
 
 const router = express.Router();
@@ -21,4 +23,20 @@ router.post('/register', async (req, res) => {
   } catch (error) {
     res.status(400).json({ error: 'User registration failed' });
   }
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) return res.status(401).json({ error: 'Invalid credentials' });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(401).json({ error: 'Invalid credentials' });
+
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, {
+    expiresIn: '1d',
+  });
+
+  res.json({ token });
 });
