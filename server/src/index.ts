a449c935995423f05cc706616ddf904b1f08d253
enkhmendeee <nergui.eegii04@gmail.com>
2025-07-22 15:31:08 +0800

    mounted protected route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import prisma from './config/prisma';
 import authRoutes from './routes/auth';
+import protectedRoutes from './routes/protected';
+
 import '../express';
 
 const app = express();
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api', protectedRoutes);
+
 app.get('/users', async (req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
