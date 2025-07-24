8de34c10f7d46beee66b1122ece98335106d7539
enkhmendeee <nergui.eegii04@gmail.com>
2025-07-24 11:09:29 +0800

    added homework routes to index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import prisma from './config/prisma';
 import authRoutes from './routes/auth';
 import protectedRoutes from './routes/protected';
+import homeworkRoutes from './routes/homework.route';
 
 const app = express();
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api', protectedRoutes);
+app.use('/api/homeworks', homeworkRoutes);
 
 app.get('/users', async (req, res) => {
   const users = await prisma.user.findMany();
