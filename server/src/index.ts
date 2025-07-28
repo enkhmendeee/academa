cace227b9ae8cdafdcdd10836dd08ac57046945a
enkhmendeee <nergui.eegii04@gmail.com>
2025-07-28 11:51:23 +0800

    added cors to adjust to client port

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,14 @@ import prisma from './config/prisma';
 import authRoutes from './routes/auth';
 import protectedRoutes from './routes/protected';
 import homeworkRoutes from './routes/homework.route';
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173', // Adjust this to your client URL
+  credentials: true,
+}));
 app.use('/api/auth', authRoutes);
 app.use('/api', protectedRoutes);
 app.use('/api/homeworks', homeworkRoutes);
