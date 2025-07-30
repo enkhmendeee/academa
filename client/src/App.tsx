7d89a367e87c48fea7cfe2d5281f0ace108cff65
enkhmendeee <nergui.eegii04@gmail.com>
2025-07-30 14:07:44 +0800

    added private route for dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route 
+        path="/dashboard" 
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        } 
+      />
     </Routes>
   );
 }
 
 export default App;
+
 
